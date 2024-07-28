0ef579513f530afb640d508bcf48bdfa5f256485
Georgia H <hager.georgia.maria@gmail.com>
2024-07-28 16:10:37 +0200

    css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -137,7 +137,6 @@ const getCurrentTheme = () =>
   document.body.classList.contains(darkTheme) ? "dark" : "light";
 const getCurrentIcon = () =>
   themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
-
 // We validate if the user previously chose a topic
 if (selectedTheme) {
   // If the validation is fulfilled, we ask what the issue was to know if we activated or deactivated the dark
